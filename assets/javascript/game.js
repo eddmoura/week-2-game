926242e9a4e3c39c306a437d1b35fd54e898c09c
[eddmoura] <edd.alca@gmail.com>
2017-05-28 15:45:20 -0400

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+var compnum = 0;
+var Wins = 0;
+var Losses = 0;
+
+function compunum(){
+compnum = Math.floor(Math.random() * 100) + 19;
+
+$("#compnum").html(compnum);
+};
+
+
+
+
+
+});
+compunum()
