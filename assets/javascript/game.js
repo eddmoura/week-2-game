3f276480dbf06df42e0f7c99f3499e8ba03b7c53
[eddmoura] <edd.alca@gmail.com>
2017-05-28 21:03:45 -0400

    Final Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,103 @@
 
 $(document).ready(function(){
 
-var compnum = 0;
-var Wins = 0;
-var Losses = 0;
+var computer = 0;
+var wins = 0;
+var losses = 0;
+var guest = 0;
+var game = true;
+var crystal = {crystal01: Math.floor(Math.random()*12 + 1), crystal02: Math.floor(Math.random()*12 +1), crystal03: Math.floor(Math.random()*12+1), crystal04: Math.floor(Math.random()*12+1)};
 
 function compunum(){
-compnum = Math.floor(Math.random() * 100) + 19;
+computer = Math.floor(Math.random() * 100) + 19;
 
-$("#compnum").html(compnum);
+$("#computer").html(computer);
+
 };
 
+function status1() {
 
 
+  if(guest === computer) {
+    wins++;
+    $("#wins").html("Wins: " + wins);
+    $('#status').html("You win!");
+    
+    game = false;
+  }
+
+  else if(guest > computer) {
+    losses++;
+    $("#losses").html("Losses:" + losses);
+    $("#status").html("You lost.");
+    
+    game = false;
+  }
+
+
+
+
+
+};
+
+compunum();
+
+$("#crystal1").on("click", function() {
+  if(game === true){
+  guest = crystal.crystal01 + guest;
+  $("#addnumber").html(guest);
+ console.log(crystal.crystal01);
+ status1();
+
+
+};
+});
+
+$("#crystal2").on("click", function(){ 
+
+  if(game === true){
+  guest = crystal.crystal02 + guest;
+  $("#addnumber").html(guest);
+  console.log(guest);
+  status1();
+  
+};
+});
+
+
+
+$("#crystal3").on("click", function() {
+  if(game === true){
+  guest = crystal.crystal03 + guest;
+  $("#addnumber").html(guest);
+  console.log(guest);
+  status1();
+
+};
+});
+
+
+$("#crystal4").on("click", function() {
+  if(game === true){
+  guest = crystal.crystal04 + guest;
+  $("#addnumber").html(guest);
+ console.log(guest);
+  status1();
+
+};
+});
+
+$("#reset").on("click", function() {
+  guest = 0;
+  $("#addnumber").html(guest);
+  $("#status").html("Status:");
+  compunum();
+  status1();
+  crystal = {crystal01: Math.floor(Math.random()*12 + 1), crystal02: Math.floor(Math.random()*12 +1), crystal03: Math.floor(Math.random()*12+1), crystal04: Math.floor(Math.random()*12+1)};
+  game = true;
+  
+  
+});
 
 
 });
-compunum()
